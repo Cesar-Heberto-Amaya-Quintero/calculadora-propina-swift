31d9175d7f4265ae1328f3c29d297fefce786045
Fer <reikafer@gmail.com>
2022-08-18 09:39:59 -0700

    ejercicio terminado

--- calculadora-propinas/ViewController.swift
+++ calculadora-propinas/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var porcentaje :Float = 0
+    
+    
+    
+    @IBOutlet weak var tfldTotal: UITextField!
+    @IBOutlet weak var lblPorcentaje: UILabel!
+    @IBOutlet weak var sldPropina: UISlider!
+    @IBOutlet weak var lblPropina: UILabel!
+    @IBOutlet weak var lblTotal: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+
+    @IBAction func doChangeValue(_ sender: Any) {
+        porcentaje = sldPropina.value
+        lblPorcentaje.text = "\(round(porcentaje))%"
+        
+        let totalStr = tfldTotal.text!
+        
+        let total = Float(totalStr) ?? 0.0
+        
+        let resultadoPropina = (total * (porcentaje/100.0))
+        
+        let cantidadTotal = total + resultadoPropina
+        
+        lblPropina.text = String(format: "$ %.2f",resultadoPropina)
+        
+        lblTotal.text = String(format: "$ %.2f",cantidadTotal)
+        
+        
+    }
 }
 
